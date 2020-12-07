9771c3c37263562d2b366222db97f7fed9ceadb0
Andreysokol2 <71218421+Andreysokol2@users.noreply.github.com>
2020-12-07 21:07:11 +0300

    2

--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 
 def update_screen(screen):
-    screen_color = (255, 0, 0)
+    screen_color = (210,229,184)
 
     screen.fill(screen_color)
 
@@ -12,4 +12,25 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                print('w')
+            if event.key == pygame.K_s:
+                print('s')
+            if event.key == pygame.K_a:
+                print('a')
+            if event.key == pygame.K_d:
+                print('d')
+            if event.key == pygame.K_q:
+                sys.exit()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                print('w')
+            if event.key == pygame.K_s:
+                print('s')
+            if event.key == pygame.K_a:
+                print('a')
+            if event.key == pygame.K_d:
+                print('d')
 
